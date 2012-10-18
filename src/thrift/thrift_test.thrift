20922d2cd106dd32e59b7b8b00b4d81f38675b24
dartov <anton.zadorozhniy@gmail.com>
2012-10-18 16:50:38 +0400

    Added thrift

---
+++ src/thrift/thrift_test.thrift
@@ -0,0 +1,12 @@
+namespace java com.mycompany.sertest.thrift
+
+
+struct SimpleThriftStructParent {
+	1: required i64 parentId
+}
+
+struct SimpleThriftStruct {
+	1: required i64 parentId,
+	2: required string message,
+	3: required i64 someId
+}
---
+++ test/jvm/com/mycompany/sertest/ThriftTest.java
@@ -0,0 +1,40 @@
+package com.mycompany.sertest;
+
+import com.mycompany.sertest.thrift.SimpleThriftStruct;
+import org.apache.thrift.TDeserializer;
+import org.apache.thrift.TException;
+import org.apache.thrift.TSerializer;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.testng.annotations.Test;
+
+public class ThriftTest
+{
+    public static final int DUMMY_ID = 12;
+    public static final int DUMMY_PARENT_ID = 34;
+
+
+    @Test
+    public void thrift10000()
+    {
+        final long time = System.currentTimeMillis();
+
+        final SimpleThriftStruct simpleInStruct = new SimpleThriftStruct(DUMMY_PARENT_ID,"message",DUMMY_ID);
+        final TSerializer _serializer = new TSerializer(new TBinaryProtocol.Factory());
+        final TDeserializer _deserializer = new TDeserializer(new TBinaryProtocol.Factory());
+        SimpleThriftStruct simpleOutStruct = new SimpleThriftStruct();
+
+
+        for (int i = 0; i < 10000; i++) {
+
+            try {
+                final byte[] _bytes = _serializer.serialize(simpleInStruct);
+                _deserializer.deserialize(simpleOutStruct,_bytes);
+
+            } catch (TException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Thrift-10000 Time = " + (System.currentTimeMillis() - time));
+    }
+}
