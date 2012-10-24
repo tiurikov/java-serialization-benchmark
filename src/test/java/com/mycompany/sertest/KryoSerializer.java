5ba90c3e7417a66b8711512898bfd64509535a9d
dartov <anton.zadorozhniy@gmail.com>
2012-10-24 13:04:41 +0400

    Added Kryo test

--- src/test/java/com/mycompany/sertest/KryoSerializer.java
+++ src/test/java/com/mycompany/sertest/KryoSerializer.java
@@ -1,5 +1,9 @@
 package com.mycompany.sertest;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
 /**
  * Created with IntelliJ IDEA.
  * User: dart
@@ -7,5 +11,27 @@ package com.mycompany.sertest;
  * Time: 12:58
  * To change this template use File | Settings | File Templates.
  */
-public class KryoSerializer {
+public final class KryoSerializer {
+
+    private static final Kryo kryo = new Kryo();
+
+    static {
+        kryo.setRegistrationRequired(false);
+    }
+
+    public static void register(Class... classes) {
+        for(Class clazz : classes) {
+            kryo.register(clazz);
+        }
+    }
+
+    public static byte[] write(Object obj) {
+        Output output = new Output(512, 1024 * 8);
+        kryo.writeClassAndObject(output, obj);
+        return output.getBuffer();
+    }
+
+    public static Object read(byte[] bytes) {
+        return kryo.readClassAndObject(new Input(bytes));
+    }
 }
--- src/test/java/com/mycompany/sertest/KryoTest.java
+++ src/test/java/com/mycompany/sertest/KryoTest.java
@@ -1,26 +1,33 @@
 package com.mycompany.sertest;
 
-import com.google.gson.Gson;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import com.mycompany.sertest.dto.SimpleDataObject;
 import org.testng.annotations.Test;
 
 /**
  *
- * @author Admin
+ * @author dart
  */
 public class KryoTest extends PerformanceTestBase
 {
     @Test
-    public void gson100000TransformationsRun()
+    public void kryo100000TransformationsRun()
     {
         final long time = System.currentTimeMillis();
-        final Gson gson = new Gson();
+
 
         for (int i = 0; i < EXECUTION_COUNT_100000; i++) {
-            final String json = gson.toJson(OBJECT_TO_TRANSFORM);
-            gson.fromJson(json, SimpleDataObject.class);
+
+           byte[] result = KryoSerializer.write(OBJECT_TO_TRANSFORM);
+           KryoSerializer.read(result);
         }
 
-        System.out.println("Gson-100000 Time = " + (System.currentTimeMillis() - time));
+        System.out.println("Kryo-100000 Time = " + (System.currentTimeMillis() - time));
     }
+
+
 }
+
+
