12235b35bd4731f9b8b58e715cadc4bf26418c3a
nirro <nirro@nirro-desktop>
2014-02-23 16:35:22 +0400

    decouple serialization and deserialization tests

--- src/test/java/com/mycompany/sertest/common/PerformanceTestBase.java
+++ src/test/java/com/mycompany/sertest/common/PerformanceTestBase.java
@@ -15,19 +15,37 @@ public abstract class PerformanceTestBase
     public static final SimpleDataObject OBJECT_TO_TRANSFORM =
             new SimpleDataObject(12, "message", new ArrayList(Arrays.asList("mac os", "linux", "windows")), 34);
 
-    @Test
-    public void transformTest() throws Exception
+    @Test(priority = 0)
+    public void serializeTest() throws Exception
     {
         final long startTime = System.currentTimeMillis();
 
         for (int i = 0; i < EXECUTION_COUNT; i++) {
-            deserialize(serialize(OBJECT_TO_TRANSFORM));
+            serialize(OBJECT_TO_TRANSFORM);
         }
 
         final long executionTime = System.currentTimeMillis() - startTime;
         final double processingSpeed = EXECUTION_COUNT / (executionTime / 1000d);
 
-        System.out.printf("%14s Time: %4s ms, Speed: %8.2f ops/sec, Record Size: %s bytes\n",
+        System.out.printf("Ser:%14s Time: %5s ms, Speed: %8.2f ops/sec, Record Size: %s bytes\n",
+                this.getClass().getSimpleName(), executionTime, processingSpeed, serialize(OBJECT_TO_TRANSFORM).length);
+    }
+    
+    @Test(priority = 1)
+    public void deserializeTest() throws Exception
+    {
+        byte[] serialized = serialize(OBJECT_TO_TRANSFORM);
+        
+        final long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < EXECUTION_COUNT; i++) {
+            deserialize(serialized);
+        }
+
+        final long executionTime = System.currentTimeMillis() - startTime;
+        final double processingSpeed = EXECUTION_COUNT / (executionTime / 1000d);
+
+        System.out.printf("Des:%14s Time: %5s ms, Speed: %8.2f ops/sec, Record Size: %s bytes\n",
                 this.getClass().getSimpleName(), executionTime, processingSpeed, serialize(OBJECT_TO_TRANSFORM).length);
     }
 
